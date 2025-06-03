239b782a6859b9a68529a163f419a534e284fb51
jhaasbroek <j.hattingh.haasbroek@student.tue.nl>
2025-06-03 14:55:39 +0200

    Add API function

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 path = "datasets"  # to store the cleaned dataset, make sure this folder exists!
 eda = False
+use_api = False
--- exogenous_variables.py
+++ exogenous_variables.py
@@ -3,9 +3,13 @@ pd.set_option('display.max_columns', None)  # show all columns
 import xlwings as xw
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+import requests
+import json
+import joblib
+from config import use_api
 
 
-def add_population(file_path: str) -> pd.DataFrame:
+def population_df(file_path: str) -> pd.DataFrame:
     """
     Create dataframe with population data from all years.
     :param file_path: path to Excel file
@@ -33,5 +37,29 @@ def add_population(file_path: str) -> pd.DataFrame:
 
     return df
 
-df_all_pop = add_population('lsoa_ward_data/land-area-population-density-london.xlsx')
+
+def new_ward_codes_df(old_codes: list[str]) -> dict[str, list]:
+    """
+    Use a JSON API to convert the 2022 ward codes to the current 2024 ward codes.
+    The API returns the same dictionary as in `population_new_ward_codes.pkl`.
+    :param old_codes: list of old ward codes
+    :return: dictionary with the old wards as keys and their new ward codes as values.
+    """
+    new_codes = {}
+
+    for c in old_codes:
+        r = requests.get(f"https://findthatpostcode.uk/areas/{c}")
+        data = json.loads(r.text)
+        new_codes[c] = data['data']['attributes']['successor']
+
+    return new_codes
+
+
+df_all_pop = population_df('lsoa_ward_data/land-area-population-density-london.xlsx')
+old_ward_codes = list(df_all_pop['Ward code'].unique())
+new_ward_codes = new_ward_codes_df(old_ward_codes) if use_api else joblib.load('lsoa_ward_data/population_new_ward_codes.pkl')
+df_all_pop['New ward code'] = df_all_pop['Ward code'].map(lambda k: new_ward_codes[k] if new_ward_codes[k] else [k])
+df_all_pop  = df_all_pop.explode('New ward code', ignore_index=True)
+cols_to_average = ['Population', 'Square Kilometres', 'Population per square kilometre']
+df_all_pop = df_all_pop.groupby(['New ward code', 'Year'])[cols_to_average].mean().reset_index()
 print(df_all_pop)
