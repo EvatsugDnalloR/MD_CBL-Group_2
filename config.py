e5f809dd486d346691d2fd9ead5ee050900d874d
jhaasbroek <j.hattingh.haasbroek@student.tue.nl>
2025-06-09 14:25:56 +0200

    Add comments to config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-path = "datasets"  # to store the cleaned dataset, make sure this folder exists!
-eda = False
-use_api = False
+path = "datasets"  # path to store datasets, make sure this folder exists!
+eda = False  # perform EDA
+use_api = False  # use API to update ward codes or use saved pickle file
+finetune = False  # fine-tune all models or use saved model orders
