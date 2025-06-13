2cc630766d4faec86ec6ef4e3faea08b89f82aa8
Gustave <gustaverolland777@gmail.com>
2025-06-13 11:55:04 +0200

    fixed some tiny issues

--- config.py
+++
@@ -1,4 +0,0 @@
-path = "data"  # path to store datasets
-eda = False  # perform EDA
-use_api = False  # use API to update ward codes or use saved pickle file
-finetune = False  # fine-tune all models or use saved model orders
