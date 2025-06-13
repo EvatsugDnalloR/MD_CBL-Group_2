dc4112d3dc0908841c2852888bc75dc0d8018172
Roberto <r.tormo.navarro@student.tue.nl>
2025-06-13 12:21:40 +0200

    Updated dashboard

--- dashboard/allocation.py
+++ dashboard/allocation.py
@@ -1,9 +1,12 @@
-def allocate_police(df_predictions):
-    """
-    Allocate police officers based on the risk factor.
-    """
-    max_pred = max(df_predictions['prediction'])
-    df_predictions['risk'] = round((df_predictions['prediction'] / max_pred), 3) # Calculate risk factor
-    df_predictions['officers'] = round(df_predictions['risk'] * 100)
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-    return df_predictions
+def allocate_police(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Allocate police officers based on burglary predictions.
+    """
+    scaler = MinMaxScaler(feature_range=(14, 100))
+    df['officers'] = np.round(scaler.fit_transform(df[['prediction']])).astype(int)
+
+    return df
--- dashboard/data.py
+++ dashboard/data.py
@@ -7,3 +7,19 @@ predictions = allocate_police(pd.read_csv("static/prediction.csv"))
 wards= pd.read_csv("static/list_london_wards.csv")
 burglary= pd.read_csv("static/structured_residential_burglary.csv")
 
+burglary['Date'] = pd.to_datetime({
+    'year': burglary['Year'],
+    'month': burglary['Month'],
+    'day': 1
+})  #Add date column for easier processing in time series graph
+
+predictions['Date'] = pd.to_datetime({
+    'year': predictions['Year'],
+    'month': predictions['Month'],
+    'day': 1
+})
+
+
+population=pd.read_csv("static/population.csv")
+occupancy=pd.read_csv("static/occupancy_rating_bedrooms_wards.csv")
+cars_vans=pd.read_csv("static/cars_or_vans_wards.csv")
